2675e375e17dcc8297ecabbf4bedaa44bba7577e
Joseph <joseph.siaojh@gmail.com>
2020-04-21 12:45:37 +0800

    feat: add css style for 'index', 'article_post', and add show posts by author, type, tag

--- miniblog/blogs/urls.py
+++ miniblog/blogs/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blogs', views.index, name='index'),
     path('blogs/post', views.index, name='index'),
-    path('blogs/post/<int:article_id>/', views.article_post, name='article_post')
+    path('blogs/post/<int:article_id>/', views.article_post, name='article_post'),
+    path('blogs/author/<str:author_name>', views.find_by_author, name='find_by_author'),
+    path('blogs/type/<str:type_name>', views.find_by_type, name='find_by_type'),
+    path('blogs/tag/<str:tag_name>', views.find_by_tag, name='find_by_tag'),
 ]
--- miniblog/blogs/views.py
+++ miniblog/blogs/views.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+from django.contrib.auth.models import User
 # from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 # from django.views.generic.base import TemplateView
 from django.template.response import TemplateResponse
 
-from .models import Article, ArticleTag
+from .models import Article, ArticleTag, ArticleType
 
 
 def index(request):
@@ -23,3 +24,33 @@ def article_post(request, article_id):
     }
 
     return render(request, 'blogs/article_post.html', context)
+
+
+def find_by_author(request, author_name):
+    article_list = User.objects.filter(username=author_name).first().article_set.all()
+
+    context = {
+        'article_list': article_list
+    }
+
+    return render(request, 'index.html', context)
+
+
+def find_by_type(request, type_name):
+    article_list = ArticleType.objects.filter(type_name=type_name).first().articles.all()
+
+    context = {
+        'article_list': article_list
+    }
+
+    return render(request, 'index.html', context)
+
+
+def find_by_tag(request, tag_name):
+    article_list = ArticleTag.objects.filter(tag_name=tag_name).first().articles.all()
+
+    context = {
+        'article_list': article_list
+    }
+
+    return render(request, 'index.html', context)
